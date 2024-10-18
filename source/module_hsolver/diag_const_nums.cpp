545d2eca4c772be3dbeeef73a9fa33364aefe4fc
Critsium <tsfxwbbzxy@163.com>
2024-10-17 23:00:25 -0400

    Refactor: Use memory_op to set diag_const_nums (#5246)
    * Use memory_op to set consts
    
    * Fix segfault
    
    * Pyabacus test
    
    * Revert changes
    
    * Modify pointer usage
    
    * I will win
    
    * Malloc test
    
    * Initialize with nullptr
    
    * Remove useless code
    
    ---------
    
    Co-authored-by: Haozhi Han <haozhi.han@stu.pku.edu.cn>

--- source/module_hsolver/diag_const_nums.cpp
+++ source/module_hsolver/diag_const_nums.cpp
@@ -9,27 +9,60 @@ template class const_nums<std::complex<float>>;
 
 // Specialize templates to support double types
 template <>
-const_nums<double>::const_nums() : zero(0.0), one(1.0), neg_one(-1.0)
+const_nums<double>::const_nums()
 {
+    base_device::memory::resize_memory_op<double, base_device::DEVICE_CPU>()(
+                        this->cpu_ctx, this->zero, 1);
+    this->zero[0] = 0.0;
+    base_device::memory::resize_memory_op<double, base_device::DEVICE_CPU>()(
+                        this->cpu_ctx, this->one, 1);
+    this->one[0] = 1.0;
+    base_device::memory::resize_memory_op<double, base_device::DEVICE_CPU>()(
+                        this->cpu_ctx, this->neg_one, 1);
+    this->neg_one[0] = -1.0;
 }
 
 // Specialize templates to support double types
 template <>
-const_nums<float>::const_nums() : zero(0.0), one(1.0), neg_one(-1.0)
+const_nums<float>::const_nums()
 {
+    base_device::memory::resize_memory_op<float, base_device::DEVICE_CPU>()(
+                        this->cpu_ctx, this->zero, 1);
+    this->zero[0] = 0.0;
+    base_device::memory::resize_memory_op<float, base_device::DEVICE_CPU>()(
+                        this->cpu_ctx, this->one, 1);
+    this->one[0] = 1.0;
+    base_device::memory::resize_memory_op<float, base_device::DEVICE_CPU>()(
+                        this->cpu_ctx, this->neg_one, 1);
+    this->neg_one[0] = -1.0;
 }
 
 // Specialized templates to support std:: complex<double>types
 template <>
 const_nums<std::complex<double>>::const_nums()
-    : zero(std::complex<double>(0.0, 0.0)), one(std::complex<double>(1.0, 0.0)),
-      neg_one(std::complex<double>(-1.0, 0.0))
 {
+    base_device::memory::resize_memory_op<std::complex<double>, base_device::DEVICE_CPU>()(
+                        this->cpu_ctx, this->zero, 1);
+    this->zero[0] = std::complex<double>(0.0, 0.0);
+    base_device::memory::resize_memory_op<std::complex<double>, base_device::DEVICE_CPU>()(
+                        this->cpu_ctx, this->one, 1);
+    this->one[0] = std::complex<double>(1.0, 0.0);
+    base_device::memory::resize_memory_op<std::complex<double>, base_device::DEVICE_CPU>()(
+                        this->cpu_ctx, this->neg_one, 1);
+    this->neg_one[0] = std::complex<double>(-1.0, 0.0);
 }
 
 // Specialized templates to support std:: complex<float>types
 template <>
 const_nums<std::complex<float>>::const_nums()
-    : zero(std::complex<float>(0.0, 0.0)), one(std::complex<float>(1.0, 0.0)), neg_one(std::complex<float>(-1.0, 0.0))
 {
+    base_device::memory::resize_memory_op<std::complex<float>, base_device::DEVICE_CPU>()(
+                        this->cpu_ctx, this->zero, 1);
+    this->zero[0] = std::complex<float>(0.0, 0.0);
+    base_device::memory::resize_memory_op<std::complex<float>, base_device::DEVICE_CPU>()(
+                        this->cpu_ctx, this->one, 1);
+    this->one[0] = std::complex<float>(1.0, 0.0);
+    base_device::memory::resize_memory_op<std::complex<float>, base_device::DEVICE_CPU>()(
+                        this->cpu_ctx, this->neg_one, 1);
+    this->neg_one[0] = std::complex<float>(-1.0, 0.0);
 }
--- source/module_hsolver/diag_const_nums.h
+++ source/module_hsolver/diag_const_nums.h
@@ -1,13 +1,15 @@
 #ifndef DIAG_CONST_NUMS
 #define DIAG_CONST_NUMS
+#include "module_base/module_device/memory_op.h"
 
 template <typename T>
 struct const_nums
 {
     const_nums();
-    T zero;
-    T one;
-    T neg_one;
+    base_device::DEVICE_CPU* cpu_ctx = {};
+    T* zero = nullptr;
+    T* one = nullptr;
+    T* neg_one = nullptr;
 };
 
 #endif
