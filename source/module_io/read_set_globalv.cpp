d7150d07d6ae94969b8785423dc8fa1745b4eab8
Yu Liu <77716030+YuLiu98@users.noreply.github.com>
2024-10-24 17:39:25 +0800

    Fix: do not bcast my_rank (#5339)

--- source/module_io/read_set_globalv.cpp
+++ source/module_io/read_set_globalv.cpp
@@ -95,7 +95,7 @@ void ReadInput::set_globalv(Parameter& para)
 
 void ReadInput::set_globalv_bcast()
 {
-    add_int_bcast(sys.myrank);
+    // add_int_bcast(sys.myrank);
     add_bool_bcast(sys.two_fermi);
     add_bool_bcast(sys.use_uspp);
     add_bool_bcast(sys.dos_setemin);
