821b18426e1caa8d1cb76cf846486e24a9cc5622
wqzhou <33364058+WHUweiqingzhou@users.noreply.github.com>
2024-10-29 13:59:03 +0900

    Feature: make SCF converges only if drho and dene is smaller than threshold (#5357)
    * SCF converges if both drho and dene is smaller than threshold
    
    * Update source/module_esolver/esolver_ks.cpp
    
    update corresponding comment
    
    Co-authored-by: dyzheng <zhengdy@dp.tech>
    
    ---------
    
    Co-authored-by: dyzheng <zhengdy@dp.tech>

--- source/module_esolver/esolver_ks.cpp
+++ source/module_esolver/esolver_ks.cpp
@@ -673,10 +673,10 @@ void ESolver_KS<T, Device>::iter_finish(int& iter)
     this->pelec->f_en.etot_old = this->pelec->f_en.etot;
 
     // add a energy threshold for SCF convergence
-    if (this->conv_esolver == 0) // only check when density is not converged
+    if (this->scf_ene_thr > 0.0 && this->conv_esolver == 1) // only check when density is converged
     {
         this->conv_esolver
-            = (iter != 1 && std::abs(this->pelec->f_en.etot_delta * ModuleBase::Ry_to_eV) < this->scf_ene_thr);
+            = (std::abs(this->pelec->f_en.etot_delta * ModuleBase::Ry_to_eV) < this->scf_ene_thr);
     }
 }
 
