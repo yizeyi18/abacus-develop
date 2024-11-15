5b1777c48b33eefa4669519072aa334027a4bf00
Mohan Chen <mohanchen@pku.edu.cn>
2024-11-15 09:14:58 +0800

    Update esolver_ks_lcao_tddft.h (#5496)

--- source/module_esolver/esolver_ks_lcao_tddft.h
+++ source/module_esolver/esolver_ks_lcao_tddft.h
@@ -1,5 +1,5 @@
-#ifndef W_ABACUS_DEVELOP_ABACUS_DEVELOP_SOURCE_MODULE_ESOLVER_ESOLVER_KS_LCAO_TDDFT_H
-#define W_ABACUS_DEVELOP_ABACUS_DEVELOP_SOURCE_MODULE_ESOLVER_ESOLVER_KS_LCAO_TDDFT_H
+#ifndef ESOLVER_KS_LCAO_TDDFT_H
+#define ESOLVER_KS_LCAO_TDDFT_H
 #include "esolver_ks.h"
 #include "esolver_ks_lcao.h"
 #include "module_elecstate/elecstate_lcao_tddft.h"
@@ -18,18 +18,22 @@ class ESolver_KS_LCAO_TDDFT : public ESolver_KS_LCAO<std::complex<double>, doubl
 
     void before_all_runners(const Input_para& inp, UnitCell& cell) override;
 
+    //! wave functions of last time step
     psi::Psi<std::complex<double>>* psi_laststep = nullptr;
 
+    //! Hamiltonian of last time step
     std::complex<double>** Hk_laststep = nullptr;
 
+    //! Overlap matrix of last time step
     std::complex<double>** Sk_laststep = nullptr;
 
-    // same as pelec
+    //! Electronic states of rt-TDDFT
     elecstate::ElecStateLCAO_TDDFT* pelec_td = nullptr;
 
     int td_htype = 1;
 
   protected:
+
     virtual void hamilt2density_single(const int istep, const int iter, const double ethr) override;
 
     virtual void update_pot(const int istep, const int iter) override;
