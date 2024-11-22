7f7a3089c69529bac483e2c66908f11b084d2bd7
Mohan Chen <mohanchen@pku.edu.cn>
2024-11-22 10:26:13 +0800

    Update gint_gamma.h (#5550)

--- source/module_hamilt_lcao/module_gint/gint_gamma.h
+++ source/module_hamilt_lcao/module_gint/gint_gamma.h
@@ -23,37 +23,28 @@ class Gint_Gamma : public Gint
 {
 	public:
 
-	// Gint_Gamma();
-	// ~Gint_Gamma();
-
-    /// @brief move operator for the next ESolver to directly use its infomation
-    /// @param rhs 
-    /// @return *this
+    //! @brief move operator for the next ESolver to directly use its infomation
+    //! @param rhs 
+    //! @return *this
     Gint_Gamma& operator=(Gint_Gamma&& rhs);
 
-    //------------------------------------------------------
-    // in gint_gamma_vl.cpp 
-    //------------------------------------------------------
-    // there is an additional step in calculating vlocal for gamma point
-    // namely the redistribution of Hamiltonian from grid to 2D block format
-    // hence we have an additional layer outside the unified interface
+    //! in gint_gamma_vl.cpp 
+    //! there is an additional step in calculating vlocal for gamma point
+    //! namely the redistribution of Hamiltonian from grid to 2D block format
+    //! hence we have an additional layer outside the unified interface
     void cal_vlocal(Gint_inout* inout, const bool new_e_iteration);
 
-    //------------------------------------------------------
-    // in gint_gamma_env.cpp 
-    //------------------------------------------------------
-	// calcualte the envelope function
+    //! in gint_gamma_env.cpp 
+	//! calcualte the electronic wave functions via grid integral
 	void cal_env(const double* wfc, double* rho,UnitCell &ucell);
 
-    //------------------------------------------------------
-    // in veff_lcao.cpp
-    //------------------------------------------------------
-    /// transfer this->hRGint to Veff::hR
+    //! transfer this->hRGint to Veff::hR
     void transfer_pvpR(hamilt::HContainer<double>* hR,const UnitCell* ucell);
 
 private:
 
-    double*** DM = nullptr;   //pointer to LOC.DM
+    //! pointer to density matrix
+    double*** DM = nullptr;
 
 };
 
